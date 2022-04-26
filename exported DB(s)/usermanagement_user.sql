08ec11488be39fa03b9f97de9f5b510a48218581
Sayuri <it20197032@my.sliit.lk>
2022-04-26 13:25:23 +0530

    user DB file added

--- exported DB(s)/usermanagement_user.sql
+++ exported DB(s)/usermanagement_user.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
 --
--- Host: localhost    Database: world
+-- Host: localhost    Database: usermanagement
 -- ------------------------------------------------------
 -- Server version	8.0.28
 
@@ -24,14 +24,14 @@ DROP TABLE IF EXISTS `user`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
   `userID` int NOT NULL AUTO_INCREMENT,
-  `userName` varchar(45) DEFAULT NULL,
-  `userNic` varchar(45) DEFAULT NULL,
-  `userEmail` varchar(45) DEFAULT NULL,
-  `userAdd` varchar(45) DEFAULT NULL,
-  `userTeleNo` varchar(45) DEFAULT NULL,
-  `AcctNo` varchar(45) DEFAULT NULL,
+  `userName` varchar(45) NOT NULL,
+  `userNic` varchar(10) NOT NULL,
+  `userEmail` varchar(45) NOT NULL,
+  `userAdd` varchar(45) NOT NULL,
+  `userTeleNo` varchar(10) NOT NULL,
+  `AcctNo` varchar(45) NOT NULL,
   PRIMARY KEY (`userID`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,9 +40,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'Saman Wijethunga','1236547891','samanW@gamil.com','No:1, Gamunu Rd. , Kadawatha','0714433689','450025334'),
-(2,'Kamal Senananyake','4523698741','kamalS@gamil.com','No: 3 , flower Rd. , Colombo 7.','0714253697','546466544'),
-(3,'Kavi Perera','6666555555','kaviP@gmail.com','123/3, blooming street, colombo 6','0114582364','333333375');
+INSERT INTO `user` VALUES (1,'Saman Wijethunga','1236547891','samanW@gamil.com','No:1, Gamunu Rd. , Kadawatha','0714433689','450025334'),(2,'Kamal Senananyake','4523698741','kamalS@gamil.com','No: 3 , flower Rd. , Colombo 7.','0714253697','546466544'),(3,'Kavi Perera','6666555555','kaviP@gmail.com','123/3, blooming street, colombo 6','0114582364','333333375');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -55,4 +53,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-04-26 12:39:42
+-- Dump completed on 2022-04-26 13:21:10
